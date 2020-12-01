cde75f76121cdde14fa798a3e77e4de00c7270cd
Noelwiz <noelwiz1@gmail.com>
2020-12-01 13:16:35 -0800

    Fixed issue #3, documenting Name

--- MyStore.Store/Name.cs
+++ MyStore.Store/Name.cs
@@ -5,12 +5,29 @@ using System.Text;
 
 namespace MyStore.Store
 {
-    public struct Name : IComparer<Name>, ISerializable
+    /// <summary>
+    /// A class to hold a customers name.
+    /// </summary>
+    public struct Name : IComparer<Name>
     {
+        /// <summary>
+        /// The first name of a customer.
+        /// </summary>
         public string First { get; }
+        /// <summary>
+        /// Middile Initial
+        /// </summary>
         public char? MiddleInitial { get; }
+        /// <summary>
+        /// Last name of the customer
+        /// </summary>
         public string Last { get; }
 
+        /// <summary>
+        /// Constructor for a name of someone w/o a middle name
+        /// </summary>
+        /// <param name="first">First Name of the customer</param>
+        /// <param name="last">Last name of the customer</param>
         public Name(string first, string last)
         {
             First = first;
@@ -18,6 +35,12 @@ namespace MyStore.Store
             MiddleInitial = null;
         }
 
+        /// <summary>
+        /// Constructor for someone with a middle initial
+        /// </summary>
+        /// <param name="first">The first name</param>
+        /// <param name="last">The last name</param>
+        /// <param name="middle">The middle initial</param>
         public Name(string first, string last, char? middle = null)
         {
             First = first;
@@ -25,7 +48,12 @@ namespace MyStore.Store
             MiddleInitial = middle;
         }
 
-
+        /// <summary>
+        /// Compare one name to another alphabetically. Used for sorting
+        /// </summary>
+        /// <param name="x">This name</param>
+        /// <param name="y">The other name</param>
+        /// <returns>A value of 1, or -1 or 0 depending on which name comes first alphabetically.</returns>
         public int Compare(Name x, Name y)
         {
             if(x.First == y.First)
@@ -67,21 +95,20 @@ namespace MyStore.Store
             }
         }
 
-        public void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            info.AddValue("First", First);
-            if(MiddleInitial != null)
-            {
-                info.AddValue("Middle", MiddleInitial);
-            }
-            info.AddValue("Last", Last);
-        }
-
+        /// <summary>
+        /// Get a string representation of the name
+        /// </summary>
+        /// <returns>The string version of the name</returns>
         public override string ToString()
         {
             return $"{First,20} {MiddleInitial?.ToString() ?? " "} {Last, 20}";
         }
 
+        /// <summary>
+        /// Compare equality to another object.
+        /// </summary>
+        /// <param name="obj">The other object</param>
+        /// <returns>True or false</returns>
         public override bool Equals(object obj)
         {
             if(obj is Name)
